12a325a101b51c88c09b5d035c815560eed49e31
Tommas Kazatel <kazatel@jiktogroup.cz>
2017-01-26 20:20:10 +0100

    fix codacy typos

--- themes/barcamp/assets/js/default.js
+++ themes/barcamp/assets/js/default.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
 
     function logo(image, old) {
 
-        var path = old.split('/');
+        var path = old.split("/");
         path[path.length - 1] = image;
-        return path.join('/');
+        return path.join("/");
 
     }
 
@@ -17,39 +17,39 @@ $(document).ready(function() {
 
             controll = true;
             $("#nav").removeClass("white-nav");
-            $("#logo-img").attr("src", logo('barcamp_black.svg', $("#logo-img").attr("src")));
+            $("#logo-img").attr("src", logo("barcamp_black.svg", $("#logo-img").attr("src")));
 
         } else if (document.body.scrollTop < 1 && controll) {
 
             controll = false;
             $("#nav").addClass("white-nav");
-            $("#logo-img").attr("src", logo('barcamp_white.svg', $("#logo-img").attr("src")));
+            $("#logo-img").attr("src", logo("barcamp_white.svg", $("#logo-img").attr("src")));
 
         }
 
     }
 
-    window.onscroll = function() { menu() }
+    $(window).scroll(function () {
+        menu();
+    });
 
     menu();
 
     $("#arrow").on('click', function(){
-        $('html,body').animate({ scrollTop: $("#where").offset().top-50}, 500);
+        $("html,body").animate({ scrollTop: $("#where").offset().top-50}, 500);
     });
 
-    $(".show-faq").on('click', function() {
+    $(".show-faq").on("click", function() {
 
-        var ps = $(this).next('p');
-        var span = $(this).prev('span');
+        var ps = $(this).next("p");
+        var span = $(this).prev("span");
 
-        console.log(span);
-
-        if(ps.is(':hidden')) {
+        if(ps.is(":hidden")) {
             ps.slideDown(300);
-            span[0].innerText = '-';
+            span[0].innerText = "-";
         } else {
             ps.slideUp(300);
-            span[0].innerText = '+';
+            span[0].innerText = "+";
         }
 
     });
